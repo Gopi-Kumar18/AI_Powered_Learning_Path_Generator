6de29c145b2fb3c6d7f1ba748aa26b12e911f392
Gopi.K-18 <gptz1811@gmail.com>
2026-03-27 19:56:42 +0530

    changed subject code-2

--- frontend-server/src/components/admin/AdminDashboard.jsx
+++ frontend-server/src/components/admin/AdminDashboard.jsx
@@ -21,11 +21,11 @@ const AdminDashboard = () => {
 
 const subjects = [
     
-    { id: 1, name: "OS" },
+    { id: 5, name: "OS" },
     { id: 2, name: "DSA" },
     { id: 3, name: "OS LAB" },
     { id: 4, name: "Verbal Ability" },
-    { id: 5, name: "Java Programming" },
+    { id: 1, name: "Java Programming" },
     { id: 6, name: "Computer Networks" },
     
 ];
